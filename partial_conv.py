fb898219ea82f113e48fea8c042159980ebc097f
Oliver Hoffman <oliver@aifi.io>
2018-05-09 19:10:50 -0700

    progress

--- partial_conv.py
+++ partial_conv.py
@@ -2,20 +2,24 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class PartialConv(nn.Conv2d):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True):
+class PartialConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+    	self.conv = nn.Conv2d(in_channels, out_channels, kernel_size)
+    	
+    	# We use broadcasting in mask operations to avoid using more channels
+    	self.mask_update = nn.Conv2d(1, 1, kernel_size)
+    	self.mask_update.weight = torch.nn.Parameter(torch.ones(self.mask_update.weight.shape))
+        self.mask_update.bias = torch.nn.Parameter(torch.zeros(self.mask_update.bias.shape))
 
-    	super(PartialConv, self).__init__(
-    		in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+    def forward(self, image, mask):
 
-    def forward(self, input):
-        # See the autograd section for explanation of what happens here.
-        return LinearFunction.apply(input, self.weight, self.bias)
+    	# Partial convolution
+    	input_tensor = image * mask * (1.0/mask.sum().item())
+    	result = self.conv(input_tensor)
+    	result[result == self.conv.bias] = 0
 
-    # def extra_repr(self):
-    #     # (Optional)Set the extra information about this module. You can test
-    #     # it by printing an object of this class.
-    #     return 'in_features={}, out_features={}, bias={}'.format(
-    #         self.in_features, self.out_features, self.bias is not None
-    #     )
+    	# Mask update
+    	mask = self.mask_update(mask)
+    	mask[mask != 0] = 1
+
+        return result, mask
